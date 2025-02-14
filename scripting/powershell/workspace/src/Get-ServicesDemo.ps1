27d7b9ed97d45701364a712dcb7ddd0a5054b671
autocloudarc <autocloudarc@outlook.com>
2025-02-14 14:26:41 -0500

    services(list): update

--- scripting/powershell/workspace/src/Get-ServicesDemo.ps1
+++ scripting/powershell/workspace/src/Get-ServicesDemo.ps1
@@ -1,16 +1,24 @@
-$services = Get-Service
+$services = Get-Service | Select-Object -First 10
 foreach ($service in $services) 
 {
     Write-Host "Service Name: $($service.Name)"
     Write-Host "Display Name: $($service.DisplayName)"
+    if ($service.Status -eq 'Running') 
+    {
+        Write-Host "Status: $($service.Status)"
+    } # end if
+    else 
+    {
+        Write-Host "Status: $($service.Status)"
+    } # end if
     Write-Host "Status: $($service.Status)"
     Write-Host "-----------------------------------"
     if ($service.Status -eq 'Running') 
     {
         Write-Host "The service is running."
-    }
+    } # end if
     else 
     {
         Write-Host "The service is not running."
-    }
-} # end foreach 
+    } # end if
+} # end foreach
