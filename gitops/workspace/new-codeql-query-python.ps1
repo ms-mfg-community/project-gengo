f4df1108339ee7a28dc62e01805aa35f8ce57672
autocloudarc <autocloudarc@outlook.com>
2025-05-03 22:36:42 -0400

    extractors(json): show

--- gitops/workspace/new-codeql-query-python.ps1
+++ gitops/workspace/new-codeql-query-python.ps1
@@ -3,3 +3,5 @@ codeql database create codeql-dbs --source-root=$(git rev-parse --show-toplevel)
 # The analysis fails because the codeql/python-queries cannot be found, which appears to be a known issue at: https://github.com/github/codeql/issues/13364
 codeql database analyze codeql-dbs --format=sarif-latest --threads=4 --sarif-category=python --output=python-results.sarif
 # codeql github upload-results --repository=ms-mfg-community/project-gengo --ref=refs/heads/main --commit=$(git rev-parse HEAD) --sarif=python-results.sarif
+# Show extractors and analyzers that are available for the language
+codeql resolve languages --format=betterjson
