8f1721a81fdcda7bb5d8b2d27bc0d485c1619be1
autocloudarc <autocloudarc@outlook.com>
2025-05-04 17:23:36 -0400

    codeql(analysis): add script for python

---
+++ gitops/codeql-queries.md
@@ -0,0 +1,4 @@
+# CodeQL Query Prompts
+
+1. What is the standard python codeql query for cross site request forgery protection that is weakened or disabled ?
+2. Show me the query syntax
--- gitops/workspace/new-codeql-query-python.ps1
+++
@@ -1,7 +0,0 @@
-# reference: https://docs.github.com/en/code-security/codeql-cli/getting-started-with-the-codeql-cli/about-the-codeql-cli#example-ci-configuration-for-codeql-analysis
-codeql database create codeql-dbs --source-root=$(git rev-parse --show-toplevel) --language=python
-# The analysis fails because the codeql/python-queries cannot be found, which appears to be a known issue at: https://github.com/github/codeql/issues/13364
-codeql database analyze codeql-dbs --format=sarif-latest --threads=4 --sarif-category=python --output=python-results.sarif
-# codeql github upload-results --repository=ms-mfg-community/project-gengo --ref=refs/heads/main --commit=$(git rev-parse HEAD) --sarif=python-results.sarif
-# Show extractors and analyzers that are available for the language
-codeql resolve languages --format=betterjson
