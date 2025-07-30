566ac93e933071935afcc8413f1917be838bd1b0
autocloudarc <autocloudarc@outlook.com>
2025-07-30 10:48:07 -0400

    secret(exclusion): subsequent push test

--- secrets-exclusion/Test-SecretsExclusion.ps1
+++ secrets-exclusion/Test-SecretsExclusion.ps1
@@ -1,4 +1,5 @@
 # This is a simple script to test the .gitattributes secret exclusion feature for GitHub Advanced Security - Secret Scanning (GitHub Secret Protection)
+# Testing subsequent push
 $password = "p@ssword123!"
 $adoToken = "3dzSjnceDcP0DZNC26nRYQKByRJ0K5BZZ1eGD0uPWyqPUDZ0VlwNJQQJ99BGACAAAAAlPKxMAAASAZDOyQJO"
 Write-Output "Secrets Exclusion Test: `$password`t$password `n`$adoToken $adoToken"
