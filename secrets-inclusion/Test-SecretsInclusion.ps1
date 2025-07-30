41e5dc50182a402d44304093d19697c0e770c6a2
autocloudarc <autocloudarc@outlook.com>
2025-07-30 08:29:39 -0400

    secret(inclusion-exclusion): test 1

---
+++ secrets-inclusion/Test-SecretsInclusion.ps1
@@ -0,0 +1,4 @@
+# This is a simple script to test the .gitattributes secret exclusion feature for GitHub Advanced Security - Secret Scanning (GitHub Secret Protection)
+$password = "MyS3cur3P@ss2025#"
+$adoToken = "7kzWmqheFgR2GZNX49oUYQKByRL8N6CZZ2hHF1vQXzrRVGZ2WmxOKRRK00CHBDAAAAAPCQN9AAASTAEGPZR1"
+Write-Output "Secrets Inclusion Test: `$password`t$password `n`$adoToken $adoToken"
