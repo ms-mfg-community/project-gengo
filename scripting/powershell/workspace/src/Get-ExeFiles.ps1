63636ea34d142e7e96ed32dcd1f1f0440349daa4
autocloudarc <autocloudarc@outlook.com>
2025-10-07 13:47:49 -0400

    minor(demos): day 1of3 ghc workshop

--- scripting/powershell/workspace/src/Get-ExeFiles.ps1
+++ scripting/powershell/workspace/src/Get-ExeFiles.ps1
@@ -1,15 +1,15 @@
-# This script recursively enumerates all folders, subfolders, and files from the current directory,
-# filtering to search for and display all files with the *.exe extension.
-# It then creates a custom class to save the output of the script and exports these results to a CSV file.
-# Finally, it imports the CSV file, sorts the results by file name in descending order, and displays them in a formatted table.
+# This script recursively counts all folders, sub-folders and files from the current directory,
+# filters to find and display all files with *.exe extension.
+# It then creates a custom class to save the script's output and exports these results to a CSV file.
+# Finally, it imports the CSV file, sorts the results in descending order by file name and displays them in a formatted table.
 
-# Recursively enumerate all folders, subfolders, and files from the current directory,
-# filtering to search for and display all files with the *.exe extension.
+# Recursively count all folders, sub-folders and files from the current directory,
+# filtering to find and display all files with *.exe extension.
 Get-ChildItem -Path $PWD -Recurse -File -Filter "*.exe" | ForEach-Object {
     Write-Output $_.FullName
 } # end ForEach 
 
-# Create a custom class to save the output of the script
+# Create a custom class to save the script's output
 class ExeFiles {
     [string[]] $Files
 }
@@ -24,5 +24,5 @@ foreach ($file in $exeFiles)
 
 Export-Csv -Path "exe_files.csv" -InputObject $exeFiles -NoTypeInformation
 
-# Import the CSV file, sort the results by file name in descending order, and display them in a formatted table.
-Import-Csv -Path "exe_files.csv" | Sort-Object -Property Files -Descending | Format-Table -AutoSize
+# Import the CSV file, sort the results in descending order by file name and display them in a formatted table.
+Import-Csv -Path "exe_files.csv" | Sort-Object -Property FileName -Descending | Format-Table
